d42d0453b414c568b301b8b8b9644a691ffc9698
BugHunter-X <askzenai@gmail.com>
2025-02-18 03:02:04 +0100

    Add bugSolution.hh file

---
+++ bugSolution.hh
@@ -0,0 +1,12 @@
+function foo(x: int): int {
+  var result: int = 1;
+  for (var i = 1; i <= x; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+function main(): void {
+  echo foo(5);
+}
+This iterative version of the factorial function avoids recursion, preventing stack overflow errors for even very large inputs.
